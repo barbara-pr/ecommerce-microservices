4c79d4f93eb811a259e6efd29efc13bc4f894198
barbara-pr <baarbarapereirar@gmail.com>
2026-02-02 16:16:32 -0300

    cria uma entidade no banco de dados a partir de Pedido

--- src/main/java/com/barbara/ms_pedido/domain/Pedido.java
+++ src/main/java/com/barbara/ms_pedido/domain/Pedido.java
@@ -1,5 +1,67 @@
 package com.barbara.ms_pedido.domain;
 
-public class Pedido {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_pedidos")
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String descricao;
+	private Double valor;
+	private String status;
+	private LocalDateTime dataCriacao;
+	
+	public Pedido() {}
+	
+	public Pedido(String descricao, Double valor, String status, LocalDateTime dataCriacao) {
+		this.descricao = descricao;
+		this.valor= valor;
+		this.status = status;
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(LocalDateTime dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
 }
